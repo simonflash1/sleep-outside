2abfaf0fc614f85db1b4c7a4d950c637b3ff3320
Christopher <edesonchristopher@gmail.com>
2025-03-14 00:48:43 +0100

    Add getParam utility function for URL parameter retrieval

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,11 @@
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -5,11 +13,10 @@ export function qs(selector, parent = document) {
 // or a more concise version if you are into that sort of thing:
 // export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localStorage
+// retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
