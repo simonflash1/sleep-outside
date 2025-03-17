80b2bc83e4e565141222ffb86b44424f919b1f78
Christopher <edesonchristopher@gmail.com>
2025-03-17 16:29:51 +0100

    Initialize product list for "tents" category in main.js

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+// Create an instance of ProductData for the "tents" category
+const productData = new ProductData("tents");
+
+// Select the element where product cards will be rendered
+const listElement = document.querySelector(".product-list");
+
+// Create and initialize a new ProductList instance
+const productList = new ProductList("tents", productData, listElement);
+productList.init();
